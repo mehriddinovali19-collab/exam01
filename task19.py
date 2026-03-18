d7d2f3c697dfe4047ca44a40af996f86408fa4f4
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-18 15:56:01 +0500

    task20 done

---
+++ task19.py
@@ -0,0 +1,7 @@
+text = "Salom dunyo"
+find = "a, e, i, o, u"
+count = 0
+for letter in text:
+ for letter in find:
+        count +=1
+print(count)
---
+++ task20.py
@@ -0,0 +1,3 @@
+
+for i in range(1, 11):
+    print(f"{i} ning kvadrati: {i**2}")
