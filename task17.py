caf58c4b2c3ab04a6ec018892889d7e78abfd9fc
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-18 15:31:20 +0500

    task17 done

---
+++ task17.py
@@ -0,0 +1,11 @@
+score = int(input("Enter your score of exam: "))
+if 90<=score<=100:
+    print("A, A'lo")
+elif 80<=score<=89:
+    print("B, Yaxshi")
+elif 70<=score<=79:
+    print("C, Qoniqarli")
+elif 60<=score<=69:
+    print("D, Qoniqarsiz")
+elif 0<=score<=59:
+    print("F, Yomon")
