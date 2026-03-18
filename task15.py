a1c789bb3e44a5f1614e3ce03510c77bb097db19
ali mekhridinov <mehriddinovali19@gmail.com>
2026-03-18 15:15:23 +0500

    task15 done

---
+++ task15.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+if num % 2 ==0:
+    print("2 ga bo'linadi")
+if num % 3==0:
+    print("3 ga bo'linadi")
+if num % 5 == 0:
+    print("5 ga bo'linadi")
+else:
+    print("bularinig hech biriga bo'linmaydi! ")
+    
